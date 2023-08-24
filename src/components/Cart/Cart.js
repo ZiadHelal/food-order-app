54a722ec3ee58067c784073cd3c4cf760ed1ae26
Ziad Helal <z.mahmoud1999@gmail.com>
2023-08-24 21:56:38 +0200

    cart: added cart component

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,41 @@
+import classes from './Cart.module.css';
+
+const DUMMY_ITEMS = [
+  {
+    id: 'm1',
+    name: 'Sushi',
+    description: 'Finest fish and veggies',
+    price: 22.99,
+  },
+  {
+    id: 'm2',
+    name: 'Schnitzel',
+    description: 'A german specialty!',
+    price: 16.5,
+  },
+];
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul>
+      {DUMMY_ITEMS.map((item) => (
+        <li>item.name</li>
+      ))}
+    </ul>
+  );
+  return (
+    <div>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>$62.44</span>
+      </div>
+      <div className={classes.action}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
