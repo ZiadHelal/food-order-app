3abe57558d399bf749d139b3421f7e6a4b8b87df
Ziad Helal <z.mahmoud1999@gmail.com>
2023-08-31 23:45:29 +0200

    added more logic for cart

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,7 +1,22 @@
-import classes from './MealItem.module.css';
+import { useContext } from 'react';
+
+import CartContext from '../../../store/cart-context';
 import MealItemForm from './MealItemForm';
 
+import classes from './MealItem.module.css';
+
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.mealName,
+      amount: amount,
+      price: props.price,
+    });
+  };
+
   return (
     <li className={classes.meal}>
       <div>
@@ -10,7 +25,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{`$${props.price.toFixed(2)}`}</div>
       </div>
       <div>
-        <MealItemForm id={props.id} />
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
