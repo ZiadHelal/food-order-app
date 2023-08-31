c82bdeeb68fbdf3ee0c1566cdf488751c116d4e9
Ziad Helal <z.mahmoud1999@gmail.com>
2023-08-31 23:28:50 +0200

    updated cart provider to handle state of the cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,11 +1,40 @@
+import { useReducer } from 'react';
+
 import CartContext from './cart-context';
 
+const initialCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action === 'ADD_ITEM') {
+    const updatedItems = state.items.concat(action.value);
+    const updatedTotalAmount =
+      state.totalAmount + action.value.price * action.value.amount;
+    return { items: updatedItems, totalAmount: updatedTotalAmount };
+  }
+  if (action === 'DEL_ITEM') {
+  }
+  return initialCartState;
+};
+
 const CartProvider = (props) => {
-  const addItemCartHandler = (item) => {};
-  const removeItemCartHandler = (item) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    initialCartState
+  );
+
+  const addItemCartHandler = (item) => {
+    dispatchCartAction({ type: 'ADD_ITEM', value: item });
+  };
+
+  const removeItemCartHandler = (item) => {
+    dispatchCartAction({ type: 'DEL_ITEM', value: item });
+  };
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemCartHandler,
     removeItem: removeItemCartHandler,
   };
